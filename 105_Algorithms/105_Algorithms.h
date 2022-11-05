95a8758801344798815e19c664d9c328da54eeac
Ziad AW <ziad.abouwasa@bakerhughes.com>
2022-11-04 20:18:11 -0600

    Init + 3 algos

---
+++ 105_Algorithms/105_Algorithms.h
@@ -0,0 +1,22 @@
+﻿// 105_Algorithms.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+
+/* This is a project based on Jonathan Boccara's CppCon 2018 talk
+ * on std:algorithm 
+ * This project shows a basic implementation of all those algorthms
+ * for my (and anyone else's) learning.
+ * Enjoy!
+ */
+
+/* List:
+ * 1. for_each
+ * ----  HEAP  -------
+ * 2.make_heap
+ */
